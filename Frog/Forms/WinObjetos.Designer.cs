d4e16a9b869bc3312017892b7cd7a234050a715d
Natanael Dagostin Ghellere <natanael@useall.com.br>
2025-06-27 15:59:47 -0300

    erros objetos

--- Frog/Forms/WinObjetos.Designer.cs
+++ Frog/Forms/WinObjetos.Designer.cs
@@ -30,13 +30,21 @@
         {
             rtSource = new RichTextBox();
             btnSpecBody = new Button();
+            tabControl1 = new TabControl();
+            tabPageCodigo = new TabPage();
+            tabPageErros = new TabPage();
+            dataGridErros = new DataGridView();
+            tabControl1.SuspendLayout();
+            tabPageCodigo.SuspendLayout();
+            tabPageErros.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridErros).BeginInit();
             SuspendLayout();
             // 
             // rtSource
             // 
-            rtSource.Location = new Point(10, 36);
+            rtSource.Location = new Point(0, 0);
             rtSource.Name = "rtSource";
-            rtSource.Size = new Size(804, 586);
+            rtSource.Size = new Size(801, 563);
             rtSource.TabIndex = 1;
             rtSource.Text = "";
             rtSource.WordWrap = false;
@@ -51,24 +59,74 @@
             btnSpecBody.UseVisualStyleBackColor = true;
             btnSpecBody.Click += btnSpecBody_Click;
             // 
+            // tabControl1
+            // 
+            tabControl1.Controls.Add(tabPageCodigo);
+            tabControl1.Controls.Add(tabPageErros);
+            tabControl1.Location = new Point(13, 39);
+            tabControl1.Name = "tabControl1";
+            tabControl1.SelectedIndex = 0;
+            tabControl1.Size = new Size(809, 591);
+            tabControl1.TabIndex = 3;
+            // 
+            // tabPageCodigo
+            // 
+            tabPageCodigo.Controls.Add(rtSource);
+            tabPageCodigo.Location = new Point(4, 24);
+            tabPageCodigo.Name = "tabPageCodigo";
+            tabPageCodigo.Padding = new Padding(3);
+            tabPageCodigo.Size = new Size(801, 563);
+            tabPageCodigo.TabIndex = 0;
+            tabPageCodigo.Text = "Código";
+            tabPageCodigo.UseVisualStyleBackColor = true;
+            // 
+            // tabPageErros
+            // 
+            tabPageErros.Controls.Add(dataGridErros);
+            tabPageErros.Location = new Point(4, 24);
+            tabPageErros.Name = "tabPageErros";
+            tabPageErros.Padding = new Padding(3);
+            tabPageErros.Size = new Size(801, 563);
+            tabPageErros.TabIndex = 1;
+            tabPageErros.Text = "Erros";
+            tabPageErros.UseVisualStyleBackColor = true;
+            // 
+            // dataGridErros
+            // 
+            dataGridErros.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridErros.Location = new Point(0, 0);
+            dataGridErros.Name = "dataGridErros";
+            dataGridErros.ReadOnly = true;
+            dataGridErros.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            dataGridErros.Size = new Size(805, 567);
+            dataGridErros.TabIndex = 0;
+            // 
             // WinObjetos
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(826, 634);
+            Controls.Add(tabControl1);
             Controls.Add(btnSpecBody);
-            Controls.Add(rtSource);
             KeyPreview = true;
             Name = "WinObjetos";
             StartPosition = FormStartPosition.CenterParent;
             Text = "Package";
             Load += WinPackage_Load;
             KeyDown += WinObjetos_KeyDown;
+            tabControl1.ResumeLayout(false);
+            tabPageCodigo.ResumeLayout(false);
+            tabPageErros.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)dataGridErros).EndInit();
             ResumeLayout(false);
         }
 
         #endregion
         private RichTextBox rtSource;
         private Button btnSpecBody;
+        private TabControl tabControl1;
+        private TabPage tabPageCodigo;
+        private TabPage tabPageErros;
+        private DataGridView dataGridErros;
     }
 }
--- Frog/Forms/WinObjetos.cs
+++ Frog/Forms/WinObjetos.cs
@@ -24,6 +24,10 @@ namespace Frog
             _nomeObjeto = nomeObjeto;
             _conn = conn;
             _type = type;
+
+            dataGridErros.Dock = DockStyle.Fill;
+            tabControl1.Dock = DockStyle.Fill;
+            rtSource.Dock = DockStyle.Fill;
         }
 
         private void WinPackage_Load(object sender, EventArgs e)
@@ -34,6 +38,7 @@ namespace Frog
                 btnSpecBody.Enabled = false;
 
             CarregarSource();
+            CarregarErros();
         }
 
         private void btnSpecBody_Click(object sender, EventArgs e)
@@ -63,6 +68,33 @@ namespace Frog
 
                 e.SuppressKeyPress = true;
             }
+        }
+
+        public void CarregarErros()
+        {
+            var query = $@" select sequence, line, position, text, attribute from USER_ERRORS where NAME = '{_nomeObjeto}'  ";
+
+            var qtdeLinhas = UtilBanco.RecuperarQuantidadeRegistrosDaQuery(query);
+
+            var reader = UtilBanco.RecuperarColunasCabecalhoDaGrid(query);
+
+            var colunas = new DataGridViewColumn[reader.FieldCount];
+            while (reader.Read())
+            {
+                for (int i = 0; i < reader.FieldCount; i++)
+                {
+                    colunas[i] = new DataGridViewTextBoxColumn();
+                    colunas[i].HeaderText = reader.GetName(i);
+                }
+                break;
+            }
+            var command = new OracleCommand(query, _conn);
+            reader = command.ExecuteReader();
+
+            dataGridErros.LimparGrid();
+            dataGridErros.AdicionarColunaCabecalho(colunas);
+            dataGridErros.AdicionarLinhasNaGrid(qtdeLinhas);
+            dataGridErros.AdicionarDadosNaGrid(reader);
         }
     }
 }
